abae038d990f6f6c9149715003b34d6eeaccb3df
SDmaN <sdman150296@gmail.com>
2016-10-10 22:57:03 +0400

    Добавлены вспомогательные типы данных

---
+++ TypeDefines.h
@@ -0,0 +1,12 @@
+#ifndef TYPEDEFINES_H
+#define TYPEDEFINES_H
+
+#include <vector>
+
+typedef char Byte;
+typedef unsigned char uByte;
+typedef unsigned short Address;
+
+typedef std::vector<Byte> ByteArray;
+
+#endif //TYPEDEFINES_H
